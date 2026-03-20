1911a63207b209750430625c1229e48af2c544ba
Manoj Kumar <f20231103@goa.bits-pilani.ac.in>
2026-03-20 16:35:37 -0400

    add streamlit config for upload size

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-st.set_option('server.maxUploadSize', 200)
 from PyPDF2 import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from openai import OpenAI
